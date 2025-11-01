1cf73bf38f20f3285476b74d2f322e29741df5c3
Vinicio Liuti <viniliuti@live.com>
2025-11-01 16:20:39 -0300

    WIP init script

---
+++ linux/Readme.md
@@ -0,0 +1,3 @@
+# All linux scripts
+
+TODO: Create script to download and populate stuff locally
---
+++ linux/cloneAndCopy.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# TODO: 
+# upload current zsh to repo
+# configure zsh with my settings
+# configure p10k theme
+
+gitPersonal=$HOME/gitPersonal2
+
+set -e
+
+echo() {
+    command echo ":: $*"
+}
+
+# if [ ! -d "$gitPersonal" ]; then
+    echo "creating $gitPersonal folder if it does not exist"
+    mkdir -p "$gitPersonal"
+# fi
+
+# if [ ! -d "$gitPersonal/dotfiles" ]; then 
+    # check git installed 
+
+    # mkdir -p "$gitPersonal/dotfiles"
+
+    # git --version >> /dev/null
+    if  git --version >> /dev/null ; then 
+        if [ ! -d "$gitPersonal/dotfiles" ]; then 
+            echo "cloning my dotfiles"
+            git clone https://github.com/viniliuti/dotfiles.git "$gitPersonal/dotfiles"
+        else 
+            echo "dotfiles already exists"
+            cd "$gitPersonal/dotfiles"
+            git pull
+        fi
+    else
+        echo "git not installed"
+    fi 
+
+# else 
+#     echo "dotfiles already exist"
+# fi
